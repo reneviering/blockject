86af5eca4e0322dbeea6d4f208ccba73c8d6b64e
René Viering <rene.viering@gmail.com>
2016-01-17 11:06:50 +0100

    Refactor.

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -9,64 +9,72 @@ var scriptSource = [
 
 const inject = () => {
 
-    var concatenatedScripts1 = scriptSource.join(',');
-    var scriptSourcesScript = `(function() { window.blockject = {}; window.blockject.scriptsToInject = '${concatenatedScripts1}'; })();`;
-    var s = document.createElement('script');
-    s.textContent = scriptSourcesScript;
-    (document.head || document.documentElement).appendChild(s);
+    const makeScriptUrlsGlobalAvailable = () => {
+        var concatenatedScripts1 = scriptSource.join(',');
+        var scriptSourcesScript = `(function() { window.blockject = {}; window.blockject.scriptsToInject = '${concatenatedScripts1}'; })();`;
+        var s = document.createElement('script');
+        s.textContent = scriptSourcesScript;
+        (document.head || document.documentElement).appendChild(s);
+    };
 
+    const injectScripts = () => {
+        var actualCode = '(' + function() {
 
-    var actualCode = '(' + function() {
-
-        var scriptsToInject = window.blockject.scriptsToInject.split(',');
-        const addScriptToDocument = (scriptToAdd) => {
-            (document.head || document.documentElement).appendChild(scriptToAdd);
-        };
-
-        const getNextScript = (scripts, currentIndex) => {
-            return (currentIndex + 1 < scripts.length) ? scripts[currentIndex + 1] : null;
-        };
-
-        var getElementType = (scriptUrl) => {
-            var dict = {
-                css() {
-                    var documentElement = document.createElement('link');
-                    documentElement.rel = 'stylesheet';
-                    documentElement.type = 'text/css';
-                    documentElement.href = scriptUrl;
-                    return documentElement;
-                },
-                js() {
-                    var documentElement = document.createElement('script');
-                    documentElement.src = scriptUrl;
-                    return documentElement;
-                }
+            var scriptsToInject = window.blockject.scriptsToInject.split(',');
+            const addScriptToDocument = (scriptToAdd) => {
+                (document.head || document.documentElement).appendChild(scriptToAdd);
             };
 
-            var items = scriptUrl.split('.');
-            var fileType = items[items.length - 1];
-            return dict[fileType]();
-        };
+            const getNextScript = (scripts, currentIndex) => {
+                return (currentIndex + 1 < scripts.length) ? scripts[currentIndex + 1] : null;
+            };
 
-        var mappedScripts = scriptsToInject.map(getElementType);
-
-        var concatenatedScripts = mappedScripts.map((script, index) => {
-            var maybeNextScript = getNextScript(mappedScripts, index);
-            if (maybeNextScript) {
-                script.onload = () => {
-                    addScriptToDocument(maybeNextScript);
+            var getElementType = (scriptUrl) => {
+                var dict = {
+                    css() {
+                            var documentElement = document.createElement('link');
+                            documentElement.rel = 'stylesheet';
+                            documentElement.type = 'text/css';
+                            documentElement.href = scriptUrl;
+                            return documentElement;
+                        },
+                        js() {
+                            var documentElement = document.createElement('script');
+                            documentElement.src = scriptUrl;
+                            return documentElement;
+                        }
                 };
-            }
-            return script;
-        });
 
-        addScriptToDocument(concatenatedScripts[0]);
+                var items = scriptUrl.split('.');
+                var fileType = items[items.length - 1];
+                return dict[fileType]();
+            };
+
+            var mappedScripts = scriptsToInject.map(getElementType);
+
+            var concatenatedScripts = mappedScripts.map((script, index) => {
+                var maybeNextScript = getNextScript(mappedScripts, index);
+                if (maybeNextScript) {
+                    script.onload = () => {
+                        addScriptToDocument(maybeNextScript);
+                    };
+                }
+                return script;
+            });
+
+            addScriptToDocument(concatenatedScripts[0]);
 
 
-    } + ')();';
-    var scriptToInject = document.createElement('script');
-    scriptToInject.textContent = actualCode;
-    (document.head || document.documentElement).appendChild(scriptToInject);
+        } + ')();';
+        var scriptToInject = document.createElement('script');
+        scriptToInject.textContent = actualCode;
+        (document.head || document.documentElement).appendChild(scriptToInject);
+    };
+
+    makeScriptUrlsGlobalAvailable();
+    injectScripts();
+
+
 };
 
 inject();
