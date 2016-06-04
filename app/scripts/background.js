4fea2691a40f081b3a07b2a3abf47e6e427aa65a
René Viering <rene.viering@gmail.com>
2016-06-04 17:13:28 +0200

    Implement simple blocking mechanism

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,12 +1,29 @@
 'use strict';
 
 // Enable chromereload by uncommenting this line:
-// import './lib/livereload';
+import './lib/livereload';
 
 chrome.runtime.onInstalled.addListener(function (details) {
   console.log('previousVersion', details.previousVersion);
 });
 
-chrome.browserAction.setBadgeText({text: '\'Allo'});
+const blockIt = true;
 
-console.log('\'Allo \'Allo! Event Page for Browser Action');
+const blackList = [
+    'http://codemade.js.org/caffeine/dist/bundle.js',
+    'http://codemade.js.org/caffeine/dist/main.css'
+];
+
+chrome.webRequest.onBeforeRequest.addListener(() => {
+  console.log('bla');
+	if(blackList.length === 0)
+	{
+		return {cancel: false};
+	}
+    return {
+        cancel: blockIt
+    };
+}, {
+    urls: blackList,
+    types: ['script', 'stylesheet']
+}, ['blocking']);
