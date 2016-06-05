06ab7c75fe25427d20d7e983318a862b14febfbf
René Viering <rene.viering@gmail.com>
2016-06-05 18:40:51 +0200

    Use Rx.js to get a list of all requests of the page

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,13 +1,13 @@
 'use strict';
-
+import Rx from 'rx';
 // Enable chromereload by uncommenting this line:
-import './lib/livereload';
+// import './lib/livereload';
 
 chrome.runtime.onInstalled.addListener(function (details) {
   console.log('previousVersion', details.previousVersion);
 });
 
-const blockIt = true;
+const blockIt = false;
 
 const blackList = [
     'http://codemade.js.org/caffeine/dist/bundle.js',
@@ -15,7 +15,6 @@ const blackList = [
 ];
 
 chrome.webRequest.onBeforeRequest.addListener(() => {
-  console.log('bla');
 	if(blackList.length === 0)
 	{
 		return {cancel: false};
@@ -27,3 +26,62 @@ chrome.webRequest.onBeforeRequest.addListener(() => {
     urls: blackList,
     types: ['script', 'stylesheet']
 }, ['blocking']);
+
+
+const createResourceLocator = () => {
+  let subscribers = [];
+
+  let resources = [];
+
+  const subscribe = subscriber => {
+    subscribers.push(subscriber);
+    return () => {
+        subscribers = subscribers.filter(s => s !== subscriber);
+    };
+  };
+
+  const addResource = resource => {
+    resources.push(resource);
+    subscribers.forEach(s => s());
+  };
+
+  const getResources = () => resources
+                              .map(r => r.url)
+                              .filter(url => (url.endsWith('js') || url.endsWith('css')) && !url.startsWith('chrome-extension'));
+
+  const resetResources = () => {
+    resources = [];
+  };
+
+  return {subscribe, addResource, getResources, resetResources};
+};
+const resourceLocator = createResourceLocator();
+
+const resources$ = new Rx.Subject();
+const allResources$ = resources$
+      .debounce(1000)
+      .subscribe(() => {
+        chrome.storage.sync.set({webRequests: resourceLocator.getResources()});
+      });
+
+
+resourceLocator.subscribe(() => {
+  resources$.onNext()
+});
+
+chrome.webRequest.onBeforeRequest.addListener(req => {
+  resourceLocator.addResource(req);
+    return {
+        cancel: false
+    };
+}, {
+    urls: [],
+    types: ['script', 'stylesheet']
+});
+
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    if (changeInfo.status === 'loading') {
+      resourceLocator.resetResources();
+    }
+});
--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -2,14 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {createStore} from 'redux';
 
-const app = (state = {text: 'blaaa'}, action) => {
+const app = (state = {webRequests: []}, action) => {
     switch(action.type) {
-      case 'ADD_BLOCK_URL':
-        return state;
-      case 'ACTIVATE':
-        return state;
-      case 'DEACTIVATE':
-        return state;
+      case 'RECEIVE_WEBREQUESTS':
+        return Object.assign({}, state, {
+          webRequests: [...state.webRequests, ...action.data]
+        });
       default:
         return state;
     }
@@ -17,8 +15,10 @@ const app = (state = {text: 'blaaa'}, action) => {
 
 const store = createStore(app);
 
+let counter = 0;
+
 store.subscribe(() => {
-  chrome.storage.sync.set(store.getState());
+  chrome.storage.sync.set({appState: store.getState()});
 });
 
 const Popup = React.createClass({
@@ -29,16 +29,38 @@ const Popup = React.createClass({
     this.unsubscribeStore = store.subscribe(() => {
       this.setState(store.getState());
     })
+    counter = 0;
   },
   componentWillUnmount() {
     this.unsubscribeStore();
   },
+  renderUrls() {
+    return this.state.webRequests.map(requestUrl => {
+      return <li>
+      {requestUrl}
+      <button>block it</button>
+      </li>;
+    })
+  },
   render() {
     return <div>
-      <div>{this.state.text}</div>
-      <div>{JSON.stringify(store.getState())}</div>
+      <ul>
+        {this.renderUrls()}
+      </ul>
+      <pre>
+        <code>
+        {JSON.stringify(this.state)}
+        </code>
+      </pre>
     </div>;
   }
 });
 
 ReactDOM.render(<Popup/>, document.getElementById('content'));
+
+chrome.storage.sync.get(data => {
+  store.dispatch({
+    type: 'RECEIVE_WEBREQUESTS',
+    data: data.webRequests
+  })
+});
