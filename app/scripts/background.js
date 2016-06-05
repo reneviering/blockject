efea2d2e33fd9801acaaf5cd22e9219dce01a4fd
René Viering <rene.viering@gmail.com>
2016-06-05 18:57:40 +0200

    First load data, then init background.js

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -2,45 +2,45 @@
 import Rx from 'rx';
 import createResourceLocator from './core/createResourceLocator.js';
 
-const blockIt = false;
-
-const blackList = [
-    // 'http://codemade.js.org/caffeine/dist/bundle.js',
-    // 'http://codemade.js.org/caffeine/dist/main.css'
-];
-
-chrome.webRequest.onBeforeRequest.addListener(req => {
-  return {cancel: blackList.length > 0};
-}, {
-    urls: blackList,
-    types: ['script', 'stylesheet']
-}, ['blocking']);
+const blackList = [];
 
 
-const resourceLocator = createResourceLocator();
-const resources$ = new Rx.Subject();
-const allResources$ = resources$
-      .debounce(1000)
-      .subscribe(() => {
-        chrome.storage.sync.set({webRequests: resourceLocator.getResources()});
-      });
+chrome.storage.sync.get(data => {
+
+  chrome.webRequest.onBeforeRequest.addListener(req => {
+    return {cancel: blackList.length > 0};
+  }, {
+      urls: blackList,
+      types: ['script', 'stylesheet']
+  }, ['blocking']);
 
 
-resourceLocator.subscribe(() => {
-  resources$.onNext()
-});
-
-chrome.webRequest.onBeforeRequest.addListener(req => {
-  resourceLocator.addResource(req);
-  return {cancel: false};
-}, {
-    urls: [],
-    types: ['script', 'stylesheet']
-});
+  const resourceLocator = createResourceLocator();
+  const resources$ = new Rx.Subject();
+  const allResources$ = resources$
+        .debounce(1000)
+        .subscribe(() => {
+          chrome.storage.sync.set({webRequests: resourceLocator.getResources()});
+        });
 
 
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    if (changeInfo.status === 'loading') {
-      resourceLocator.resetResources();
-    }
+  resourceLocator.subscribe(() => {
+    resources$.onNext()
+  });
+
+  chrome.webRequest.onBeforeRequest.addListener(req => {
+    resourceLocator.addResource(req);
+    return {cancel: false};
+  }, {
+      urls: [],
+      types: ['script', 'stylesheet']
+  });
+
+
+  chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+      if (changeInfo.status === 'loading') {
+        resourceLocator.resetResources();
+      }
+  });
+
 });
